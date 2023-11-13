f230349194a6d796b3d8ef25815ef5804f8a31bf
dzakye <150271646+dzakye@users.noreply.github.com>
2023-11-13 22:25:05 +0100

    tornens en deri

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders the WORD MASTER title', () => {
   render(<App />)
-  const title = screen.getByText(/CHANPYON DI MÒSE/i)
+  const title = screen.getByText(/WORD MASTER/i)
   expect(title).toBeInTheDocument()
 })
