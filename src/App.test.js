0ef96be425a74e25b561045d7e60078a88a58584
dzakye <150271646+dzakye@users.noreply.github.com>
2023-11-15 15:17:15 +0100

    RWE DI MO

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders the WORD MASTER title', () => {
   render(<App />)
-  const title = screen.getByText(/WORD MASTER/i)
+  const title = screen.getByText(/RWE DI MO DE SEN/i)
   expect(title).toBeInTheDocument()
 })
